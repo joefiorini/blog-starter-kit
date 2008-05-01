60f49c019f406bdce8f9784bbfb8bafd95098892
Joe Fiorini <joe@joe-fiorinis-macbook-pro.local>
2008-05-01 02:09:40 -0400

    Implemented Rakismet spam filtering on comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,7 @@
 class CommentsController < ResourceController::Base
   belongs_to :post
-
-	create.wants.html { redirect_to @post }
+	has_rakismet
 	
+	create.wants.html { redirect_to @post }
+	create.failure.wants.html { redirect_to @post }
 end
--- app/models/Comment.rb
+++ app/models/Comment.rb
@@ -1,7 +1,16 @@
 require 'vendor/plugins/acts_as_commentable/lib/Comment'
 class Comment
+	has_rakismet :author => 'name',
+								:author_email => 'email',
+								:author_url => 'url',
+								:content => 'comment'
 	
 	def comment_type
 		"Comment"
 	end
+	
+	def before_create
+		!self.spam?
+	end
+	
 end
